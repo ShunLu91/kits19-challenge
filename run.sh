aa4d2530d68371fc50e997d9bf64f838aafd552c
lushun_mac <470651748@qq.com>
2020-04-29 09:21:57 +0800

    train

--- run.sh
+++ run.sh
@@ -7,4 +7,5 @@
 CUDA_VISIBLE_DEVICES=7 nohup python -u train_res_unet.py -e 100 -b 32 -l 0.0001 -g 4 -s 512 512 \
         -d "/home/workspace/lushun/dataset/KiTS19/npy" \
         --log "logdir/log_ResUNet" --eval_intvl 5 --cp_intvl 5 \
-        --vis_intvl 0 --num_workers 8 &
+        --vis_intvl 0 --num_workers 8 \
+        > logdir/log_resunet 2>&1 &
